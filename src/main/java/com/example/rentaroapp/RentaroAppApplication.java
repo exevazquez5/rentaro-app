2cf4236b15019748665cc98056353c4e32b3afeb
exevazquez5 <exevazquez5@gmail.com>
2026-04-27 20:44:08 -0300

    primer commit

---
+++ src/main/java/com/example/rentaroapp/RentaroAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentaroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaroAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentaroAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentaroapp/RentaroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentaroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaroAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
